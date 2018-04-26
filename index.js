71b0b99056ff1a0e494024fe25a4ef701db2a1fd
Michael R. McGown <mrmcgown@gmail.com>
2018-04-26 19:01:56 +0000

    Done.

--- index.js
+++ index.js
@@ -9,23 +9,23 @@ function nestedTarget() {
 
 
 function increaseRankBy(n) {
-  const rankedLists = document.getElementById('app').querySelectorAll('ul.ranked-list li')
+  const rankedLists = document.querySelectorAll('.ranked-list')
   const firstList = rankedLists[0]
   const secondList = rankedLists[1]
   let children = firstList.children
-  let start = 1
+
   for (let i = 0, l = children.length; i < l; i++) {
-        return (parseInt(children[i].innerHTML) + n) 
+        children[i].innerHTML = parseInt(children[i].innerHTML) + n 
   }
   children = secondList.children
-  start = 12
+  
 
       for (let i = 0, l = children.length; i < l; i++) {
-       return (parseInt(children[i].innerHTML) + n)
+       children[i].innerHTML = parseInt(children[i].innerHTML) + n
 } console.log(rankedLists)
 }
 
-increaseRankBy(2)
+
 
 function deepestChild() {
   return document.querySelector('#grand-node div div div div')
