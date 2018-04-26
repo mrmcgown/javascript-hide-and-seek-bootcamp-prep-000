8dc9f3c332bd59c307f52d895c1dcb839f7a87ad
Michael R. McGown <mrmcgown@gmail.com>
2018-04-26 19:03:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ function increaseRankBy(n) {
   const firstList = rankedLists[0]
   const secondList = rankedLists[1]
   let children = firstList.children
+<<<<<<< HEAD
 
   for (let i = 0, l = children.length; i < l; i++) {
         children[i].innerHTML = parseInt(children[i].innerHTML) + n 
@@ -26,6 +27,21 @@ function increaseRankBy(n) {
 }
 
 
+=======
+  let start = 1
+  for (let i = 0, l = children.length; i < l; i++) {
+        return (parseInt(children[i].innerHTML) + n) 
+  }
+  children = secondList.children
+  start = 12
+
+      for (let i = 0, l = children.length; i < l; i++) {
+       return (parseInt(children[i].innerHTML) + n)
+} console.log(rankedLists)
+}
+
+increaseRankBy(2)
+>>>>>>> 6ebae293fddc4bf248fff82ab1f84a7aa85596f0
 
 function deepestChild() {
   return document.querySelector('#grand-node div div div div')
